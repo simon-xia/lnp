f4e4b38b6b8a0b487d38c0ddb4d22d589f6dfa4d
“simon-xia” <simonxiaOMG@gmail.com>
2014-06-11 14:55:49 +0800

    6.9

--- simon.sh
+++ simon.sh
@@ -3,5 +3,6 @@ gcc -o client_iterative client_iterative.c simon_socket.c -Wall
 gcc -o server_iterative server_iterative.c simon_socket.c -Wall
 gcc -o server_mutiprocess server_mutiprocess.c simon_socket.c -Wall
 gcc -o server_mutithread server_mutithread.c simon_socket.c -Wall -lpthread
-#gcc -o pool_test pthread_pool.c pool_test.c -Wall -lpthread
+gcc -o server_select server_select.c simon_socket.c -Wall
+gcc -o pool_test pthread_pool.c pool_test.c -Wall -lpthread
 gcc -o server_thrpool server_thrpool.c pthread_pool.c simon_socket.c -Wall -lpthread -g
