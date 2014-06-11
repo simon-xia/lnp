caab53f79221cd72f049ed16d2eb053b77c4316c
“simon-xia” <simonxiaOMG@gmail.com>
2014-06-11 22:46:21 +0800

    little change

--- simon.sh
+++ simon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 gcc -o client_iterative client_iterative.c simon_socket.c -Wall
 gcc -o server_iterative server_iterative.c simon_socket.c -Wall
-gcc -o server_mutiprocess server_mutiprocess.c simon_socket.c -Wall
-gcc -o server_mutithread server_mutithread.c simon_socket.c -Wall -lpthread
+gcc -o server_multiprocess server_multiprocess.c simon_socket.c -Wall
+gcc -o server_multithread server_multithread.c simon_socket.c -Wall -lpthread
 gcc -o server_select server_select.c simon_socket.c -Wall
 gcc -o pool_test pthread_pool.c pool_test.c -Wall -lpthread
 gcc -o server_thrpool server_thrpool.c pthread_pool.c simon_socket.c -Wall -lpthread -g
