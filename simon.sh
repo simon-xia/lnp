0309818f616432c15f526ded168e6427ad972336
“simon-xia” <simonxiaOMG@gmail.com>
2014-06-12 11:05:42 +0800

    i/o

--- simon.sh
+++ simon.sh
@@ -4,5 +4,6 @@ gcc -o server_iterative server_iterative.c simon_socket.c -Wall
 gcc -o server_multiprocess server_multiprocess.c simon_socket.c -Wall
 gcc -o server_multithread server_multithread.c simon_socket.c -Wall -lpthread
 gcc -o server_select server_select.c simon_socket.c -Wall
+gcc -o server_epoll server_epoll.c simon_socket.c -Wall
 gcc -o pool_test pthread_pool.c pool_test.c -Wall -lpthread
 gcc -o server_thrpool server_thrpool.c pthread_pool.c simon_socket.c -Wall -lpthread -g
